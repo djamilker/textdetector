d4018a05adabcf3b291796863270257711215d0a
djamilker <djamilkerrouche@gmail.com>
2025-04-11 11:54:13 +0300

    add proxy.js route

---
+++ api/proxy.js
@@ -0,0 +1,16 @@
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    const backendResponse = await fetch('https://text-detector-api.onrender.com/predict', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(req.body)
+    });
+
+    const data = await backendResponse.json();
+    res.status(backendResponse.status).json(data);
+  } else {
+    res.status(405).json({ error: 'Method not allowed' });
+  }
+}
